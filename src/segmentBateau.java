20c0f14176457ef78c8639f72c5d0de4edba7e59
guilhem <guilhem@LoRdi>
2016-03-17 14:38:10 +0100

    Update

--- src/segmentBateau.java
+++ src/segmentBateau.java
@@ -1,4 +1,19 @@
 public class segmentBateau extends cases {
 
     private bateau appartientA;
+
+    public segmentBateau(IHMImage touche, IHMImage normal) {
+        super(touche, normal);
+    }
+    
+    @Override
+    public  void toucher() {
+        if(touche == false)
+        appartientA.touche();
+        touche = true;
+    }
+    
+    public void getStatut() {
+     if(touche == false)
+         }    
 }
