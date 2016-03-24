e10bb768c1645c471c470318867b3d4ba447b116
guilhem <guilhem@LoRdi>
2016-03-24 15:44:09 +0100

    GPT

--- src/ordinateur.java
+++ src/ordinateur.java
@@ -1,7 +1,12 @@
+
+import java.awt.Dimension;
+import java.util.Random;
+
+
 public class ordinateur extends joueur {
 
     private int niveauDifficulte;
-
+    
     public ordinateur() {
         
     }
@@ -10,6 +15,7 @@ public class ordinateur extends joueur {
             switch (niveauDeDifficulté){
                 case 1 :{
                     // kamoulox
+                    // GNIK2TAMER
                 }
                 case 2:{
                     // TEST
@@ -17,6 +23,7 @@ public class ordinateur extends joueur {
                 case 3:{
                     // GPT
                     //lul
+                    
                 }
                 default:{
                     System.out.print("Ok ta mer");
@@ -24,4 +31,14 @@ public class ordinateur extends joueur {
             }
         }  
     }
+    
+   private int[] randomTarget(plateau carte){
+        Dimension mapSize = carte.getMapSize(); 
+        Random rand = new Random();
+        int x = rand.nextInt(mapSize.width + 1); //Nombre aléatoire entre 0 et n-1   
+        int y = rand.nextInt(mapSize.height + 1); //Entre 0 et n-1   
+        int[] tabCoordonnées = {x,y};     
+    
+        return tabCoordonnées;
+   }
 }
